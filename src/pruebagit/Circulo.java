61b0d419b1e5f2e159ea4cfd18efe8db88e17c73
Camon <neen1967123@hotmail.com>
2022-08-17 10:22:58 -0300

    Sexto commit main

--- src/pruebagit/Circulo.java
+++ src/pruebagit/Circulo.java
@@ -1,12 +1,8 @@
 package pruebagit;
 
 public class Circulo {
-<<<<<<< HEAD
 		private Double radio = 25;
-=======
-		private Double radio;
 public void X(int Y) {
 	System.out.println("a");
 };
->>>>>>> nuevarama
 }
